e706c6162a1421a2b2a7dcbcd3a2295f855da1e1
Panu <>
2018-08-07 16:29:00 +0300

    Added Finite State Machine to Bird.gd

--- Entities/Bird/Bird.gd
+++ Entities/Bird/Bird.gd
@@ -1,24 +1,107 @@
 extends RigidBody2D
 
+enum State {
+	STATE_FLYING,
+	STATE_FLAPPING,
+	STATE_FALLING,
+	STATE_GROUNDED
+}
+
 export (int) var flap_force = 150
 export (int) var move_speed = 50
 
+var current_state
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	linear_velocity = Vector2(move_speed, linear_velocity.y)
+	set_state(STATE_FLAPPING)
 
 func _process(delta):
-	if rotation_degrees < -30:
-		rotation_degrees = -30
-		angular_velocity = 0
-	
-	if linear_velocity.y > 0:
-		angular_velocity = 1.5
+	current_state._process(delta)
 
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("click"):
-		flap()
+	current_state._unhandled_input(event)
 
-func flap():
-	linear_velocity = Vector2(linear_velocity.x, -flap_force)
-	angular_velocity = -3
+func set_state(new_state):
+#	current_state.exit()
+	
+	match new_state:
+		STATE_FLYING:
+			current_state = FlyingState.new(self)
+		STATE_FLAPPING:
+			current_state = FlappingState.new(self)
+		STATE_FALLING:
+			current_state = FallingState.new(self)
+		STATE_GROUNDED:
+			current_state = GroundedState.new(self)
+		_:
+			print("Bird.set_state() Error: State not found!")
+			get_tree().quit()
+
+func get_state():
+	match current_state:
+		STATE_FLYING:
+			return STATE_FALLING
+		STATE_FLAPPING:
+			return STATE_FLAPPING
+		STATE_FALLING:
+			return STATE_FALLING
+		STATE_GROUNDED:
+			return STATE_GROUNDED
+		_:
+			print("Bird.get_state() Error: State not found!")
+			get_tree().quit()
+
+class FlyingState:
+	
+	var bird
+	
+	func _init(bird):
+		# Called when this class gets instanced.
+		self.bird = bird
+	
+	func _process(delta):
+		pass
+	
+	func _unhandled_input(event):
+		pass
+
+class FlappingState:
+	
+	var bird
+	
+	func _init(bird):
+		# Called when this class gets instanced
+		self.bird = bird
+		bird.linear_velocity = Vector2(bird.move_speed, bird.linear_velocity.y)
+	
+	func _process(delta):
+		if bird.rotation_degrees < -30:
+			bird.rotation_degrees = -30
+			bird.angular_velocity = 0
+	
+		if bird.linear_velocity.y > 0:
+			bird.angular_velocity = 1.5
+
+	func _unhandled_input(event):
+		if Input.is_action_just_pressed("click"):
+			flap()
+
+	func flap():
+		bird.linear_velocity = Vector2(bird.linear_velocity.x, -bird.flap_force)
+		bird.angular_velocity = -3
+
+class FallingState:
+	
+	var bird
+	
+	func _init(bird):
+		# Called when this class gets instanced.
+		self.bird = bird
+
+class GroundedState:
+	
+	var bird
+	
+	func _init(bird):
+		# Called when this class gets instanced.
+		self.bird = bird
